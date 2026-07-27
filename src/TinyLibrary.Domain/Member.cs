984549004a45cf619f859e9990a6486ea2f42994
Javier Cervera <franciscojavier.cervera7@gmail.com>
2026-07-28 00:39:30 +0200

    Clase Member y tests unitarios

---
+++ src/TinyLibrary.Domain/Member.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyLibrary.Domain
+{
+    public class Member
+    {
+        private readonly Guid id;
+        private readonly string name;
+        private readonly string email;
+        private readonly DateTimeOffset joinedAt;
+        private DateTimeOffset? penalizedUntil;
+
+        public Guid Id => id;
+
+        public string Name => name;
+
+        public string Email => email;
+
+        public DateTimeOffset JoinedAt => joinedAt;
+
+        public DateTimeOffset? PenalizedUntil => penalizedUntil;
+
+        public Member(string _name, string _email, DateTimeOffset _joinedAt)
+        {
+            id = Guid.NewGuid();
+            name = _name;
+            email = _email;
+            joinedAt = _joinedAt;
+        }
+
+        public void Penalize(DateTimeOffset until)
+        {
+            penalizedUntil = until;
+        }
+
+        public bool IsPenalized(DateTimeOffset now)
+        {
+            return penalizedUntil > now;
+        }
+    }
+}
---
+++ tests/TinyLibrary.Domain.Tests/MemberTests.cs
@@ -0,0 +1,55 @@
+﻿namespace TinyLibrary.Domain.Tests
+{
+    public class MemberTests
+    {
+        [Test]
+        public void Penalize_SetsPenalizedUntil()
+        {
+            var member = new Member("Test Name", "test@test.com", DateTime.Now.AddDays(-1));
+
+            DateTimeOffset expectedPenalizedUntil = DateTime.Now.AddDays(7);
+
+            member.Penalize(expectedPenalizedUntil);
+
+            Assert.That(member.PenalizedUntil, Is.EqualTo(expectedPenalizedUntil));
+
+        }
+
+        [Test]
+        public void IsPenalized_ReturnsTrue_WhenPenalizedUntilIsInFuture()
+        {
+            var member = new Member("Test Name", "test@test.com", DateTime.Now.AddDays(-1));
+
+            DateTimeOffset expectedPenalizedUntil = DateTime.Now.AddDays(1);
+
+            member.Penalize(expectedPenalizedUntil);
+
+            Assert.That(member.IsPenalized(DateTime.Now), Is.True);
+        }
+
+        [Test]
+        public void IsPenalized_ReturnsFalse_WhenPenalizedUntilIsInPast()
+        {
+            var member = new Member("Test Name", "test@test.com", DateTime.Now.AddDays(-2));
+
+            DateTimeOffset expectedPenalizedUntil = DateTime.Now.AddDays(-1);
+
+            member.Penalize(expectedPenalizedUntil);
+
+            Assert.That(member.IsPenalized(DateTime.Now), Is.False);
+
+        }
+
+        [Test]
+        public void IsPenalized_ReturnsFalse_WhenNeverPenalized()
+        {
+            var member = new Member("Test Name", "test@test.com", DateTime.Now.AddDays(-1));
+
+            Assert.That(member.IsPenalized(DateTime.Now), Is.False);
+
+        }
+
+
+
+    }
+}
